58f269c8b6f323ef259e5a87047411743bb944d2
graslowsnail <graslowsnail@gmail.com>
2025-06-12 18:33:15 -0400

    wrong url for auth client

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,7 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
     /** The base URL of the server (optional if you're using the same domain) */
-    baseURL: process.env.NODE_ENV === "production" ? "https://your-domain.com" : "http://localhost:3000"
+    baseURL: process.env.NODE_ENV === "production" ? "https://chat-bot-blond-sigma-57.vercel.app/" : "http://localhost:3000"
 });
 
 export const {
